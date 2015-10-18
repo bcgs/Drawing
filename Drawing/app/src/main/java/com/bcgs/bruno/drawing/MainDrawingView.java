3f5417a4e580094497c812f202b816ea54273054
bcgs <bruno.bcgs@gmail.com>
2015-10-17 21:23:19 -0300

    Loading image and drawing over it

---
+++ Drawing/app/src/main/java/com/bcgs/bruno/drawing/MainDrawingView.java
@@ -0,0 +1,53 @@
+package com.bcgs.bruno.drawing;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Path;
+import android.util.AttributeSet;
+import android.view.MotionEvent;
+import android.view.View;
+
+public class MainDrawingView extends View {
+
+    private Paint paint = new Paint();
+    private Path path = new Path();
+
+    public MainDrawingView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+
+        paint.setAntiAlias(true);               //Smooths out the edges
+        paint.setStrokeWidth(5f);               //Stroke width
+        paint.setColor(Color.BLACK);
+        paint.setStyle(Paint.Style.STROKE);
+        paint.setStrokeJoin(Paint.Join.ROUND);
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        canvas.drawPath(path, paint);
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+
+        float eventX = event.getX();
+        float eventY = event.getY();
+
+        switch (event.getAction()) {
+            case MotionEvent.ACTION_DOWN:
+                path.moveTo(eventX, eventY);
+                return true;
+            case MotionEvent.ACTION_MOVE:
+                path.lineTo(eventX, eventY);
+                break;
+            default:
+                return false;
+        }
+
+        // Makes our view repaint and call onDraw
+        invalidate();
+        return true;
+    }
+}
